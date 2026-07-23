f5492c2e8647a536321dca4c7a4e893f9779a79d
HARDIK <hardikdhamija676@gmail.com>
2026-07-23 14:00:29 +0530

    COMMIT

---
+++ src/ARRAYS/binarysearch.java
@@ -0,0 +1,31 @@
+package ARRAYS;
+
+public class binarysearch {
+    public static int Binarysearch(int []arr,int key){
+        int start=0;
+        int end=arr.length;
+
+        while(start<=end){
+            int mid=(start+end)/2;
+
+            if(arr[mid]==key){
+                return mid;
+
+            }
+            if(arr[mid]<key){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return -1;
+
+    }
+
+    public static void main(String[] args) {
+        int [] arr={2,4,6,8,10,12,14,16,18,20};
+        int key=4;
+        System.out.println("index at which key is present=" + Binarysearch(arr,key));
+    }
+}
---
+++ src/ARRAYS/pairs.java
@@ -0,0 +1,19 @@
+package ARRAYS;
+
+public class pairs {
+    public static void main(String[] args) {
+        int [] arr={2,4,6,8,10};
+        int pair=0;
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i+1; j < arr.length; j++) {
+
+                System.out.print("(" + arr[i] + "," + arr[j] + ")");
+                   pair++;
+            }
+            System.out.println();
+
+        }
+        System.out.println("NUMBER OF PAIRS="+pair);
+    }
+}
