0992539b50996a487b2c72cac34e99f8e7572e80
HARDIK <hardikdhamija676@gmail.com>
2026-08-17 17:09:47 +0530

    COMMIT

---
+++ src/STRINGS/reversestring.java
@@ -0,0 +1,16 @@
+package STRINGS;
+
+
+public class reversestring {
+    public static void main(String[] args) {
+
+        String s = "Hardik";
+        String rev = "";
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            rev += s.charAt(i);
+        }
+
+        System.out.println(rev);
+    }
+}
