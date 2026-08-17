ea87ff96e74507159966bef42d136a14eb0b4813
HARDIK <hardikdhamija676@gmail.com>
2026-08-17 16:35:26 +0530

    COMMIT

---
+++ src/STRINGS/equals.java
@@ -0,0 +1,9 @@
+package STRINGS;
+
+public class equals {
+    public static void main(String[] args) {
+        String s="hardik";
+        String a="hardik";
+        System.out.println(s.equals(a));
+    }
+}
