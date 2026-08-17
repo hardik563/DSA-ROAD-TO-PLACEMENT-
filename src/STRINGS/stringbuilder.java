12a840a2434fbc3ea18fad7f7704aafff60a202c
HARDIK <hardikdhamija676@gmail.com>
2026-08-17 17:00:21 +0530

    COMMIT

---
+++ src/STRINGS/stringbuilder.java
@@ -0,0 +1,8 @@
+package STRINGS;
+
+public class stringbuilder {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("abc");
+        System.out.println(sb);
+    }
+}
