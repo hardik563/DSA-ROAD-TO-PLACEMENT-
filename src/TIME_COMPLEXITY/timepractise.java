3db1ceb2c37810ffaf06d25c512389cf140312fd
HARDIK <hardikdhamija676@gmail.com>
2026-08-18 10:21:44 +0530

    COMMIT

--- src/TIME_COMPLEXITY/timepractise.java
+++ src/TIME_COMPLEXITY/timepractise.java
@@ -3,7 +3,7 @@ package TIME_COMPLEXITY;
 public class timepractise {
     public static void main(String[] args) {
         int c=0;
-        int n=20;
+        int n=5;
         for (int i = 0; i < n; i++) {
             c++;
 
@@ -14,7 +14,7 @@ public class timepractise {
 
         for (int i = 0; i < n; i+=i) {
             // for this also tc will be O(log n)
-
+            System.out.println(i);
         }
 
     }
