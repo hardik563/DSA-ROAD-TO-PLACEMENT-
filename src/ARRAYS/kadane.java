f52aa62b914a21aa3e41598097f797b8bdab0719
HARDIK <hardikdhamija676@gmail.com>
2026-08-18 20:20:41 +0530

    COMMIT

--- src/ARRAYS/kadane.java
+++ src/ARRAYS/kadane.java
@@ -8,6 +8,8 @@ public class kadane {
         for(int i=0;i<arr.length;i++){
             cs=cs+arr[i];
             if(cs<0){
+
+                {
             cs=0;
         }
         ms=Math.max(cs,ms);
