f657c9a198cf3598ea413b76b9148178e7cbc26d
HARDIK <hardikdhamija676@gmail.com>
2026-08-18 20:12:20 +0530

    COMMIT

--- src/TIME_COMPLEXITY/time.java
+++ src/TIME_COMPLEXITY/time.java
@@ -44,6 +44,7 @@ public class time {
             // it is faster
 
         }
+        
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.println("hello");
