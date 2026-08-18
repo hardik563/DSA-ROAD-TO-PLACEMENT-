b762ce9d6963c6117a4cad5ac78e391bdb6c5a61
HARDIK <hardikdhamija676@gmail.com>
2026-08-18 20:12:51 +0530

    COMMIT

--- src/ARRAYS/merge2array.java
+++ src/ARRAYS/merge2array.java
@@ -14,7 +14,6 @@ public class merge2array {
             if(arr[i]<arr1[j]) {
                 o[k] = arr[i];
                 i++;
-
             }
             else{
                 o[k]=arr1[j];
