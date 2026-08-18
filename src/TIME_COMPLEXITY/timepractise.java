e5de243d2d0d44f8a80fc4f9e1f7fd102f8c50de
HARDIK <hardikdhamija676@gmail.com>
2026-08-18 10:22:57 +0530

    COMMIT

--- src/TIME_COMPLEXITY/timepractise.java
+++ src/TIME_COMPLEXITY/timepractise.java
@@ -3,7 +3,7 @@ package TIME_COMPLEXITY;
 public class timepractise {
     public static void main(String[] args) {
         int n=5;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < =n; i++) {
 
             System.out.print(i);
         }
