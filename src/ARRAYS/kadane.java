aac0d23990a718806cb6abcac48e27fd9b7fc78d
HARDIK <hardikdhamija676@gmail.com>
2026-08-18 20:20:02 +0530

    COMMIT

--- src/ARRAYS/kadane.java
+++ src/ARRAYS/kadane.java
@@ -13,7 +13,6 @@ public class kadane {
     }
         System.out.println("MAXIMUM SUM OF ARRAY IS "+ms);
 }
-
     public static void main(String[] args) {
         int arr[]={-2,-3,4,-1,-2,1,5,-3};
         Kadanes(arr);
