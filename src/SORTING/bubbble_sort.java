ce93e31ca9aee2b911eb998887fcdf999122de18
HARDIK <hardikdhamija676@gmail.com>
2026-08-19 10:51:37 +0530

    COMMIT

--- src/SORTING/bubbble_sort.java
+++ src/SORTING/bubbble_sort.java
@@ -5,7 +5,7 @@ public class bubbble_sort {
     //{5,2,4,3,1]
     //increasing[1,2,3,4,5]
     //decreasing[5,4,3,2,1]
-    //compare one byu one and send first big at possible last
+    //compare one by one and send first big at possible last
     public static void bubbleSort(int[] arr){
         for (int turn=0;turn<arr.length-1;turn++){
            for(int j=0;j<arr.length-1-turn;j++){
