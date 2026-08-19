f12fa19b779f35ad13d6ee8f25de754162757f55
HARDIK <hardikdhamija676@gmail.com>
2026-08-19 10:47:25 +0530

    COMMIT

--- src/SORTING/bubbble_sort.java
+++ src/SORTING/bubbble_sort.java
@@ -21,9 +21,9 @@ public class bubbble_sort {
     }
     public static void intArr(int arr[]){
         for (int i = 0; i <arr.length ; i++) {
-            System.out.println(arr[i]);
+            System.out.print(arr[i]);
         }
-
+        System.out.println();
     }
 
     public static void main(String[] args) {
