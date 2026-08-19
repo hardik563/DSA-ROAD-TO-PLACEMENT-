d8cc297dd85a41cbade1e55a57a572be718a709a
HARDIK <hardikdhamija676@gmail.com>
2026-08-19 10:29:13 +0530

    COMMIT

---
+++ src/SORTING/sorting.java
@@ -0,0 +1,15 @@
+package SORTING;
+
+public class sorting {
+    public static void main(String[] args) {
+        // arrange in an particular order
+        //increasing orer
+        //decreasing order
+        // we will learn
+        //bubble sort
+        //selection sort
+        //insertion sort
+        //counting sort
+        //an many more
+    }
+}
