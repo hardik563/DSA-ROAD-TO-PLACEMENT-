179f25ad7dc98955b6a9610cbfc1e4259788b16a
HARDIK <hardikdhamija676@gmail.com>
2026-08-19 11:02:37 +0530

    COMMIT

--- src/SORTING/bubbble_sort.java
+++ src/SORTING/bubbble_sort.java
@@ -14,7 +14,6 @@ public class bubbble_sort {
            for(int j=0;j<arr.length-1-turn;j++){
                //swap of largest and pushing it to last
               if(arr[j]>arr[j+1]){
-
                   int temp=arr[j];
                   //the one which is largest always go at last in first turn
                   arr[j]=arr[j+1];
