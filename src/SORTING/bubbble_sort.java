7a71e9a911bbd0a6cf45f8e5322981dfb36ca3ae
HARDIK <hardikdhamija676@gmail.com>
2026-08-19 10:42:39 +0530

    COMMIT

--- src/SORTING/bubbble_sort.java
+++ src/SORTING/bubbble_sort.java
@@ -7,6 +7,11 @@ public class bubbble_sort {
     //decreasing[5,4,3,2,1]
     //compare one byu one and send first big at possible last
     public static void bubbleSort(int arr[]){
-        
+
+
+    }
+
+    public static void main(String[] args) {
+        int arr[]={5,4,1,3,2};
     }
 }
