0784a38c26418f6453dbfe7dd2be2d35872c8cb2
HARDIK <hardikdhamija676@gmail.com>
2026-08-20 10:14:26 +0530

    COMMIT

--- src/SORTING/checksortedarrayornot.java
+++ src/SORTING/checksortedarrayornot.java
@@ -1,23 +1,27 @@
 package SORTING;
 
 public class checksortedarrayornot {
+
     public static void main(String[] args) {
 
-
-        //check array sorted or not
-        int[] arr = {1, 2, 3, 4, 5};
+        // check array sorted or not
+        int[] arr = {1, 2, 3, 5, 4, 6};
         int n = arr.length;
-        for (int i = 0; i < n; i++) {
+
+        boolean sorted = true;
+
+        for (int i = 0; i < n - 1; i++) {
+
             if (arr[i] > arr[i + 1]) {
+                sorted = false;
                 break;
-
             }
-
-            else{
-                System.out.println("ARRAY IN SORTED:");
-            }
-
         }
 
+        if (sorted) {
+            System.out.println("ARRAY IS SORTED");
+        } else {
+            System.out.println("ARRAY IS NOT SORTED");
+        }
     }
 }
