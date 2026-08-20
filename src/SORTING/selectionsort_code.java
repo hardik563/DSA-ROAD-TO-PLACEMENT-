3e353fef30342c8647c9add2ab46d162bcf034b7
HARDIK <hardikdhamija676@gmail.com>
2026-08-20 10:30:36 +0530

    COMMIT

--- src/SORTING/selectionsort_code.java
+++ src/SORTING/selectionsort_code.java
@@ -4,4 +4,12 @@ public class selectionsort_code {
     //for i to n-2
     //inner loop
     //i+1 to n-1
+    public void selectionsort(int[]arr){
+        for(int i=0;i<arr.length-1;i++){
+            int minPos=i;
+            for(int j=i+1;j<arr.length-1;j++){
+
+            }
+        }
+    }
 }
