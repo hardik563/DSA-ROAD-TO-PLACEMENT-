cdb327cd686331848fecb735a83b1111a3151e42
HARDIK <hardikdhamija676@gmail.com>
2026-08-20 10:26:01 +0530

    COMMIT

---
+++ src/SORTING/selectiona_sort.java
@@ -0,0 +1,11 @@
+package SORTING;
+
+public class selectiona_sort {
+    public static void main(String[] args) {
+        System.out.println("SELECTION SORT");
+        //in this we will pick the smallest element
+        //and put it at the beginning
+        //in same say we do
+        //and at last the array will be sorted array
+    }
+}
