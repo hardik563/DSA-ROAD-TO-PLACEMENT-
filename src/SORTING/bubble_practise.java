8dd0c5bbffa35e58154cab61dc53298073de0505
HARDIK <hardikdhamija676@gmail.com>
2026-08-20 09:55:41 +0530

    COMMIT

--- src/SORTING/bubble_practise.java
+++ src/SORTING/bubble_practise.java
@@ -1,16 +1,29 @@
 package SORTING;
 
 public class bubble_practise {
-    public static void bubbleeee(int [] arr){
+    public static void bubbleeee(int [] arr) {
 
-        for(int turn=0;turn<arr.length-1;turn++){
-            for(int j=0;j<arr.length-1-turn;j++){
-                if(arr[j]>arr[j+1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=arr[j];
+        for (int turn = 0; turn < arr.length - 1; turn++) {
+            for (int j = 0; j < arr.length - 1 - turn; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = arr[j];
                 }
             }
         }
+        System.out.println("AFTER USING BUBBLE SORT THE ARRAY WILL BE:");
+    }
+
+        public static  void printArr(int []arr){
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+            System.out.println();
+    }
+
+    public static void main(String[] args) {
+
     }
 }
