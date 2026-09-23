18d4f9e80f6252c7ba3814e8acc893f9348fbcca
HARDIK <hardikdhamija676@gmail.com>
2026-09-23 14:14:00 +0530

    COMMIT

--- src/list/listtt.java
+++ src/list/listtt.java
@@ -12,7 +12,6 @@ public class listtt {
         Collections.sort(list);
         System.out.println("Ascending order: " + list);
 
-
         Collections.sort(list, Collections.reverseOrder());
         System.out.println("Descending order: " + list);
     }
